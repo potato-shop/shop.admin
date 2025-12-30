400820cece7c63c787decff754d90b65bb833e21
gary <yellow90249@gmail.com>
2025-12-30 16:42:39 +0800

    feat: test

--- src/components/Cropper.vue
+++ src/components/Cropper.vue
@@ -1,16 +1,18 @@
 <template>
-  <cropper-canvas background>
-    <cropper-image :src="imageSrc" ref="imageRef" rotatable scalable skewable translatable></cropper-image>
-    <cropper-shade hidden></cropper-shade>
+  <cropper-canvas background style="height: 300px">
+    <cropper-image :src="imageSrc" ref="imageRef" rotatable scalable translatable crossorigin="anonymous"></cropper-image>
     <cropper-handle action="select" plain></cropper-handle>
-    <cropper-selection ref="selectionRef" initial-coverage="0.5" movable resizable>
+    <cropper-selection
+      :initialAspectRatio="1"
+      :aspectRatio="1"
+      :initialCoverage="1"
+      ref="selectionRef"
+      initial-coverage="0.5"
+      movable
+      resizable
+    >
       <cropper-grid role="grid" covered></cropper-grid>
-      <cropper-crosshair centered></cropper-crosshair>
       <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
-      <cropper-handle action="n-resize"></cropper-handle>
-      <cropper-handle action="e-resize"></cropper-handle>
-      <cropper-handle action="s-resize"></cropper-handle>
-      <cropper-handle action="w-resize"></cropper-handle>
       <cropper-handle action="ne-resize"></cropper-handle>
       <cropper-handle action="nw-resize"></cropper-handle>
       <cropper-handle action="se-resize"></cropper-handle>
@@ -18,6 +20,8 @@
     </cropper-selection>
   </cropper-canvas>
   <button @click="getCropData">取得資料</button>
+  <!-- 這裡 -->
+  <img :src="croppedImageSrc" alt="裁切後的圖片" v-if="croppedImageSrc" style="max-width: 300px; margin-top: 20px;">
 </template>
 
 <script setup>
@@ -28,8 +32,9 @@ const imageSrc = ref(
   'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200'
 );
 const selectionRef = ref(null);
+const croppedImageSrc = ref('');
 
-const getCropData = () => {
+const getCropData = async () => {
   if (selectionRef.value) {
     console.log('🚀 ~ getCropData ~ selectionRef.value):', selectionRef.value);
     console.log({
@@ -37,6 +42,14 @@ const getCropData = () => {
       y: selectionRef.value.y,
       width: selectionRef.value.width,
       height: selectionRef.value.height,
+    });
+
+    // 獲取裁切後的圖片
+    const canvas = await selectionRef.value.$toCanvas();
+    canvas.toBlob((blob) => {
+      if (blob) {
+        croppedImageSrc.value = URL.createObjectURL(blob);
+      }
     });
   }
 };
